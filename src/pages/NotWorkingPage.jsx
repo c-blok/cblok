958985bd590b11495e9dd8619904a78793173a41
artemijsS <artjom1408@gmail.com>
2022-04-11 22:47:52 +0300

    notWorkingPage

---
+++ src/pages/NotWorkingPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Helmet } from "react-helmet"
+import { useSelector } from "react-redux"
+
+import logo from "../assets/svg/logo.svg"
+
+function NotWorkingPage () {
+
+    const language = useSelector(({ language }) => language.language)
+
+    return (
+        <div>
+            <Helmet>
+                <title>{text.title[language]}</title>
+            </Helmet>
+            <div className="error">
+                <img src={logo} alt="error"/>
+                <h1>{text.h1[language]}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default NotWorkingPage;
+
+const text = {
+    title: {
+        ru: "Временно не доступен | C-BLOCK",
+        lv: "Īslaicīgi nav pieejams | C-BLOCK"
+    },
+    h1: {
+        ru: "Временно не доступен",
+        lv: "Īslaicīgi nav pieejams"
+    }
+}
