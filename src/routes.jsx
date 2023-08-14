ede09559f6c9268778675ac109cc209188969e7e
artemijsS <artjom1408@gmail.com>
2023-08-14 19:11:32 +0300

    website open

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,13 +8,13 @@ export const useRoutes = () => {
 
     return (
         <Routes>
-            <Route path="/" element={ <NotWorkingPage/> }/>
-            {/*<Route path="/services" element={ <ServicesPage/> }/>*/}
-            {/*<Route path="/forhome" element={ <ForHomePage/> }/>*/}
-            {/*<Route path="/forflat" element={ <ForFlatPage/> }/>*/}
-            {/*<Route path="/projects" element={ <ProjectsPage/> }/>*/}
-            {/*<Route path="/contacts" element={ <ContactsPagePage/> }/>*/}
-            {/*<Route path="/media/logo" element={<img src={logo} alt="error"/> }/>*/}
+            <Route path="/" element={ <HomePage/> }/>
+            <Route path="/services" element={ <ServicesPage/> }/>
+            <Route path="/forhome" element={ <ForHomePage/> }/>
+            <Route path="/forflat" element={ <ForFlatPage/> }/>
+            <Route path="/projects" element={ <ProjectsPage/> }/>
+            <Route path="/contacts" element={ <ContactsPagePage/> }/>
+            <Route path="/media/logo" element={<img src={logo} alt="error"/> }/>
             <Route path="*" element={<Navigate to ="/" />}/>
         </Routes>
     )
