360f09112f9ba4c687c95e94787294cbcd10e180
artemijsS <artjom1408@gmail.com>
2023-09-06 17:08:20 +0300

    mobile

--- src/components/sections/ContactsSection.tsx
+++ src/components/sections/ContactsSection.tsx
@@ -16,8 +16,8 @@ export const ContactsSection = ({ locale } : {locale: string}) => {
                             <h2>{t("title")}</h2>
                             <h3>{t("h3")}</h3>
                             <p>{t("pCom")}</p>
-                            <p><Link href={"tel:+37120004922"}>{t("phone")} +371 20 004 922</Link></p>
-                            <p><Link href={"mailto:cblok@inbox.lv"}>{t("email")} cblok@inbox.lv</Link></p>
+                            <p><Link href={"tel:+37120004922"}>+371 20 004 922</Link></p>
+                            <p><Link href={"mailto:cblok@inbox.lv"}>cblok@inbox.lv</Link></p>
                             <iframe
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d139177.94111927054!2d23.964270853565075!3d56.971836327880276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfb0e5073ded%3A0x400cfcd68f2fe30!2zUsSrZ2E!5e0!3m2!1slv!2slv!4v1693988545244!5m2!1slv!2slv"
                                 width="97%" height="400" style={{border:0}} allowFullScreen={false} loading="lazy"
