b92737f32bbdbf7e943eb5ccc6e3534d9b6dc8b6
artemijsS <artjom1408@gmail.com>
2023-09-23 19:23:34 +0300

    text fixes & map zoom

--- src/components/sections/ContactsSection.tsx
+++ src/components/sections/ContactsSection.tsx
@@ -38,11 +38,14 @@ export const ContactsSection = ({ locale } : {locale: string}) => {
                                         <a href={"mailto:cblok@inbox.lv"}>cblok@inbox.lv</a>
                                     </span>
                                 </p>
-                                <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d139177.94111927054!2d23.964270853565075!3d56.971836327880276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfb0e5073ded%3A0x400cfcd68f2fe30!2zUsSrZ2E!5e0!3m2!1slv!2slv!4v1693988545244!5m2!1slv!2slv"
-                                    width="97%" height="400" style={{border:0}} allowFullScreen={false} loading="lazy"
-                                    referrerPolicy="no-referrer-when-downgrade"
-                                />
+                                <iframe width="97%" height="400" style={{border:0}} allowFullScreen={false}
+                                        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=riga+(My%20Business%20Name)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                                </iframe>
+                                {/*<iframe*/}
+                                {/*    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d139177.94111927054!2d23.964270853565075!3d56.971836327880276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfb0e5073ded%3A0x400cfcd68f2fe30!2zUsSrZ2E!5e0!3m2!1slv!2slv!4v1693988545244!5m2!1slv!2slv&z=15"*/}
+                                {/*    width="97%" height="400" style={{border:0}} allowFullScreen={false} loading="lazy"*/}
+                                {/*    referrerPolicy="no-referrer-when-downgrade"*/}
+                                {/*/>*/}
                             </div>
                         </div>
                         <div className={style.formBLock}>
